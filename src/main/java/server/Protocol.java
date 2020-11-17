8cc77920de5c16b76a0566e07df4e2edf3d657bd
Simon Nöjd <simon.nojd.jobb@gmail.com>
2020-11-17 12:00:47 +0100

    Added states in protocol

--- src/main/java/server/Protocol.java
+++ src/main/java/server/Protocol.java
@@ -4,9 +4,11 @@ public class Protocol {
     private static final int LOGIN = 0;
     private static final int MAIN_MENU = 1;
     private static final int WAITING_FOR_OPPONENT = 2;
-    private static final int SEND_QUESTION = 3;
-    private static final int CHECK_ANSWER = 4;
-    private static final int FINAL_SCORE = 5;
+    private static final int OVERVIEW = 3;
+    private static final int CHOOSE_CATEGORY = 4;
+    private static final int SEND_QUESTION = 5;
+    private static final int CHECK_ANSWER = 6;
+    private static final int FINAL_SCORE = 7;
 
 
     private int state = LOGIN;
@@ -17,6 +19,9 @@ public class Protocol {
     String fromClient;
     Object output;
     int counter = 0;
+    int roundCounter = 0;
+    int userRoundCounter = 2;
+    int userQuestionCounter = 4;
 
 
     public Object processInput(Object object) {
@@ -24,28 +29,39 @@ public class Protocol {
 
 
         if (state == LOGIN) {
+            objectToSend = "hej";
             System.out.println(object.toString());
             state = SEND_QUESTION;
         } else if (state == MAIN_MENU) {
-
+            state = WAITING_FOR_OPPONENT;
         } else if (state == WAITING_FOR_OPPONENT) {
-
+            state = OVERVIEW;
+        } else if (state == OVERVIEW) {
+            state = CHOOSE_CATEGORY;
+        } else if (state == CHOOSE_CATEGORY) {
+            objectToSend = database.chooseCategory(fromClient);
+            state = SEND_QUESTION;
         } else if (state == SEND_QUESTION) {
             System.out.println("Är i SendQuestion");
             objectToSend = database.test.get(counter);
-
             state = CHECK_ANSWER;
         } else if (state == CHECK_ANSWER) {
             response.setSuccess(database.findCorrectAnswer(object.toString()));
             objectToSend = response;
-            if (counter < 4) {
+            if (counter < userQuestionCounter) {
                 state = SEND_QUESTION;
                 counter++;
-            } else
+            } else if (roundCounter < userRoundCounter){
+                state = OVERVIEW;
+                roundCounter++;
+                counter = 0;
+            }
+            else {
                 state = FINAL_SCORE;
+            }
 
         } else if (state == FINAL_SCORE) {
-
+            state = MAIN_MENU;
         }
 
 
