d75ca62113dad44d3f146a28d976f8a1e1efc3e1
Patrik Melander <71705832+PatrikJMelander@users.noreply.github.com>
2020-11-18 09:06:11 +0100

    Revert "Add files via upload"

--- src/main/java/server/Questions.java
+++ src/main/java/server/Questions.java
@@ -1,17 +1,15 @@
-package server.Categories;
+package server;
 
 import java.io.Serializable;
 
 /**
  * Created by Patrik Melander
- * Changed by Axel Reje
- * Date: 2020-11-14
+ * Date: 2020-11-12
  * Time: 13:21
  * Project: Quiskampen
  * Copyright: MIT
  */
 public class Questions implements Serializable {
-    private String category;
     private String question;
     private String correctAnswer;
     private String wrongAnswer1;
@@ -19,34 +17,14 @@ public class Questions implements Serializable {
     private String wrongAnswer3;
 
 
-    public Questions(String question, String correctAnswer, String wrongAnswer1, String wrongAnswer2,
-                     String wrongAnswer3) {
+    public Questions(String question, String correctAnswer, String wrongAnswer1, String wrongAnswer2, String wrongAnswer3) {
         this.question = question;
         this.correctAnswer = correctAnswer;
         this.wrongAnswer1 = wrongAnswer1;
         this.wrongAnswer2 = wrongAnswer2;
         this.wrongAnswer3 = wrongAnswer3;
     }
-    
-    Questions(String category, String question, String correctAnswer, String wrongAnswer1, String wrongAnswer2,
-                     String wrongAnswer3)
-    {
-        this.category = category;
-        this.question = question;
-        this.correctAnswer = correctAnswer;
-        this.wrongAnswer1 = wrongAnswer1;
-        this.wrongAnswer2 = wrongAnswer2;
-        this.wrongAnswer3 = wrongAnswer3;
-        
-    }
-    
-    
-    public String getCategory()
-    {
-        return category;
-        
-    }
-    
+
     public String getQuestion() {
         return question;
     }
@@ -55,15 +33,15 @@ public class Questions implements Serializable {
         return correctAnswer;
     }
 
-    public String getWrongAnswer0() {
+    public String getWrongAnswer1() {
         return wrongAnswer1;
     }
 
-    public String getWrongAnswer1() {
+    public String getWrongAnswer2() {
         return wrongAnswer2;
     }
 
-    public String getWrongAnswer2() {
+    public String getWrongAnswer3() {
         return wrongAnswer3;
     }
 }
