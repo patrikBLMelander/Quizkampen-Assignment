ddfdf88bf7ecb6666c892860f98b597df64aa5b6
PatrikJMelander <patrikjmelander@gmail.com>
2020-11-27 14:59:10 +0100

    code cleanup

--- src/main/java/client/Controllers/LogInController.java
+++ src/main/java/client/Controllers/LogInController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 
 public class LogInController{
     ScreenNavigator s = new ScreenNavigator() ;
