c00eaa7d9addec73bec8f89fd36fda3d61235678
Max J <14187946+m4xiJo@users.noreply.github.com>
2020-11-27 10:29:31 +0100

    Post Waiting view

---
+++ src/main/java/client/Controllers/PostWaitingController.java
@@ -0,0 +1,15 @@
+package client.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class PostWaitingController {
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Button logInButton;
+
+}
